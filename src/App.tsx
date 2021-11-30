c178749c947e70629eeaa63564ade5b87713aa89
hyunjiyoo <solteee23@gmail.com>
2021-11-30 16:06:57 +0900

    navbar markup and css styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
-import React from 'react';
-import Home from './components/home/home';
 import Navbar from './components/navbar/navbar';
-import About from './components/about/about';
 import styles from './app.module.css';
-
 
 function App(): JSX.Element {
   return (
     <div className={styles.container}>
-      <Home />
       <Navbar />
-      <About />
     </div>
   );
 }
