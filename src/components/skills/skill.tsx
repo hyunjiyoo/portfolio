3e97cd4baec5ad33f73e11467f90fc0aec101f3a
hyunjiyoo <solteee23@gmail.com>
2021-12-02 11:02:49 +0900

    skills markup and styling

---
+++ src/components/skills/skill.tsx
@@ -0,0 +1,7 @@
+// import styles from './skills.module.css';
+
+const Skill = () => (
+  <h1>skill</h1>      
+);
+
+export default Skill;
--- src/components/skills/skills.tsx
+++ src/components/skills/skills.tsx
@@ -1,7 +1,99 @@
-import React from 'react';
+import styles from './skills.module.css';
 
 const Skills = (): JSX.Element => (
-  <h1>Skills</h1>
+  <section id={styles.skills}>
+    <h1 className={styles.title}>SKILLS</h1>
+    <p className={styles.title__desc}>
+      이곳은 skills 섹션을 설명하는 곳입니다.<br />
+      이곳은 skills 섹션을 설명하는 곳입니다. 이곳은 skills 섹션을 설명하는 곳입니다.
+    </p>
+    <div className={styles.skillset}>
+      <div className={styles.skillset__left}>
+        <h2 className={styles.skillset__frontend}>Front-end</h2>
+        
+        <div className={styles.skill}>
+          <h3 className={styles.skill__title}>CSS</h3>
+          <div className={styles.skill__desc}>
+            <div className={styles.skill__bar}>
+              <div className={styles.skill__value}></div>
+            </div>
+            <span>99%</span>
+          </div>
+        </div>
+
+        <div className={styles.skill}>
+          <h3 className={styles.skill__title}>Javascript</h3>
+          <div className={styles.skill__desc}>
+            <div className={styles.skill__bar}>
+              <div className={styles.skill__value}></div>
+            </div>
+            <span>99%</span>
+          </div>
+        </div>
+
+        <div className={styles.skill}>
+          <h3 className={styles.skill__title}>React</h3>
+          <div className={styles.skill__desc}>
+            <div className={styles.skill__bar}>
+              <div className={styles.skill__value}></div>
+            </div>
+            <span>99%</span>
+          </div>
+        </div>
+
+        <div className={styles.skill}>
+          <h3 className={styles.skill__title}>Typescript</h3>
+          <div className={styles.skill__desc}>
+            <div className={styles.skill__bar}>
+              <div className={styles.skill__value}></div>
+            </div>
+            <span>99%</span>
+          </div>
+        </div>
+
+      </div>
+      <div className={styles.skillset__right}>
+        <div className={styles.skillset__backend}>
+          <h2>Back-end</h2>
+
+          <div className={styles.skill}>
+          <h3 className={styles.skill__title}>Nodejs</h3>
+          <div className={styles.skill__desc}>
+            <div className={styles.skill__bar}>
+              <div className={styles.skill__value}></div>
+            </div>
+            <span>99%</span>
+          </div>
+
+          <div className={styles.skill}>
+          <h3 className={styles.skill__title}>Python</h3>
+          <div className={styles.skill__desc}>
+            <div className={styles.skill__bar}>
+              <div className={styles.skill__value}></div>
+            </div>
+            <span>99%</span>
+          </div>
+        </div>
+
+        </div>
+        </div>
+        <div className={styles.skillset__infra}>
+          <h2>Infra</h2>
+
+          <div className={styles.skill}>
+          <h3 className={styles.skill__title}>Docker</h3>
+          <div className={styles.skill__desc}>
+            <div className={styles.skill__bar}>
+              <div className={styles.skill__value}></div>
+            </div>
+            <span>99%</span>
+          </div>
+        </div>
+        </div>
+      </div>
+    </div>
+
+  </section>
 );
 
 export default Skills;
