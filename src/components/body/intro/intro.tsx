0266c7e294ef9b943d2171819213184e8422cf7a
hyunjiyoo <solteee23@gmail.com>
2021-12-06 17:48:11 +0900

    contact me 버튼 기능추가

--- src/components/body/intro/intro.tsx
+++ src/components/body/intro/intro.tsx
@@ -1,18 +1,26 @@
 import styles from './intro.module.css';
 
-const Intro = (): JSX.Element => (
-  <section id="intro" className={`${styles.intro} container`}>
-    <div className={styles.contents}>
-      <span className={styles.span}>___ Hello Hyunji World!</span>
-      <h1 className={styles.title}>
-        FRONT-END<br/>
-        DEVELOPER,<br/>
-        who have good habits.
-      </h1>
-      <button className={styles.button}>CONTACT ME</button>
-    </div>
-    <img className={styles.img} src="/images/profile.png" alt="profile" />
-  </section>
-);
+const Intro = (): JSX.Element => {
+
+  const moveContactSection = () => {
+    const contact = document.querySelector('#contact') as HTMLElement;
+    contact.scrollIntoView({behavior: 'smooth'});
+  }
+
+  return (
+    <section id="intro" className={`${styles.intro} container`}>
+      <div className={styles.contents}>
+        <span className={styles.span}>___ Hello Hyunji World!</span>
+        <h1 className={styles.title}>
+          FRONT-END<br/>
+          DEVELOPER,<br/>
+          who have good habits.
+        </h1>
+        <button className={styles.button} onClick={moveContactSection}>CONTACT ME</button>
+      </div>
+      <img className={styles.img} src="/images/profile.png" alt="profile" />
+    </section>
+  )
+};
 
 export default Intro;
