cc5d82fc6e81de16af4fd67ee909d88491798c0f
hyunjiyoo <solteee23@gmail.com>
2021-12-06 14:29:02 +0900

    navbar 상단고정

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -3,27 +3,29 @@ import styles from './navbar.module.css';
 import { faBars, faGem } from '@fortawesome/free-solid-svg-icons'
 
 const Navbar = (): JSX.Element => (
-  <nav id={styles.navbar} className={styles.section}>
-    <div className={styles.logo}>
-      <FontAwesomeIcon icon={faGem} />
-      <a className={styles.name} href="#top">
-        <span id={styles.firstName}>HYUNJI.</span>
-        <span id={styles.secondName}>YOO</span>
-      </a>
+  <nav id={styles.navbar}>
+    <div className={styles.section}>
+      <div className={styles.logo}>
+        <FontAwesomeIcon icon={faGem} />
+        <a className={styles.name} href="#top">
+          <span id={styles.firstName}>HYUNJI.</span>
+          <span id={styles.secondName}>YOO</span>
+        </a>
+      </div>
+      <ul className={styles.menu}>
+        <li className={`${styles.menu_item} ${styles.active}`} data-link="#home">Home</li>
+        <li className={styles.menu_item} data-link="#about">About</li>
+        <li className={styles.menu_item} data-link="#note">Note</li>
+        <li className={styles.menu_item} data-link="#skills">Skills</li>
+        <li className={styles.menu_item} data-link="#work">Work</li>
+        <li className={styles.menu_item} data-link="#testimonial">Testimonial</li>
+        <li className={styles.menu_item} data-link="#contact">Contact</li>
+        <li className={styles.menu_item} data-link="#social">Social</li>
+      </ul>
+      <button className={styles.button}>
+        <FontAwesomeIcon icon={faBars} />
+      </button>
     </div>
-    <ul className={styles.menu}>
-      <li className={`${styles.menu_item} ${styles.active}`} data-link="#home">Home</li>
-      <li className={styles.menu_item} data-link="#about">About</li>
-      <li className={styles.menu_item} data-link="#note">Note</li>
-      <li className={styles.menu_item} data-link="#skills">Skills</li>
-      <li className={styles.menu_item} data-link="#work">Work</li>
-      <li className={styles.menu_item} data-link="#testimonial">Testimonial</li>
-      <li className={styles.menu_item} data-link="#contact">Contact</li>
-      <li className={styles.menu_item} data-link="#social">Social</li>
-    </ul>
-    <button className={styles.button}>
-      <FontAwesomeIcon icon={faBars} />
-    </button>
   </nav>
 );
 
