2a36e671b7d9a90596e3fd38ecb55e95250c25a7
hyunjiyoo <yoohyunji@yooui-MacBookPro.local>
2021-12-13 11:15:17 +0900

    네비게이션 스크롤 active 스타일링 기능 구현

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,19 +1,27 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import styles from './navbar.module.css';
 import { faBars, faGem } from '@fortawesome/free-solid-svg-icons'
+import { useEffect } from 'react';
+import { updateExpressionWithTypeArguments } from 'typescript';
 
 const Navbar = (): JSX.Element => {
+  useEffect(() => {
 
-  const scrollIntoView = (selector: string) => {
-    const element = document.querySelector(selector) as HTMLLIElement;
-    element.scrollIntoView({behavior: 'smooth'});
-  }
-  
-  const activeEffect = (target: HTMLLIElement) => {
-    const prevTarget = document.querySelector(`.${styles.menu_item}.${styles.active}`) as HTMLLIElement;
-    prevTarget.classList.remove(`${styles.active}`);
-    target.classList.add(`${styles.active}`);
-  }
+    let observer;
+    const options = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0.5
+    };
+
+    sections = sectionIds.map(sectionId => (document.querySelector(sectionId))) as HTMLElement[];
+    navItems = sectionIds.map(sectionId => (document.querySelector(`[data-link="${sectionId}"]`))) as HTMLElement[];  
+
+    sections.forEach((section: HTMLElement) => {
+      observer = new IntersectionObserver(handleIntersect, options);
+      observer.observe(section);
+    });
+  });
 
   const scrollTo = (event: React.MouseEvent<HTMLUListElement>) => {
     const target = event.target as HTMLLIElement;
@@ -54,5 +62,70 @@ const Navbar = (): JSX.Element => {
     </nav>
   )
 };
+
+const sectionIds: string[] = [
+  '#intro',
+  '#about',
+  '#note',
+  '#skills',
+  '#work',
+  '#testimonials',
+  '#contact'
+];
+
+let sections : HTMLElement[];
+let navItems : HTMLElement[];
+let selectedNavIndex : number = 0;
+
+const scrollIntoView = (selector: string) => {
+  const element = document.querySelector(selector) as HTMLLIElement;
+  element.scrollIntoView({behavior: 'smooth'});
+}
+
+const activeEffect = (target: HTMLElement) => {
+  const prevTarget = document.querySelector(`.${styles.menu_item}.${styles.active}`) as HTMLLIElement;
+  prevTarget.classList.remove(`${styles.active}`);
+  target.classList.add(`${styles.active}`);
+}
+
+const handleIntersect = (entries: IntersectionObserverEntry[], observer: IntersectionObserver) => {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting && entry.intersectionRatio > 0) {
+      
+      const index = sectionIds.indexOf(`#${entry.target.id}`);
+      if (entry.boundingClientRect.y < 0) {
+        selectedNavIndex = (index < navItems.length - 1) ? index + 1: index;
+      } 
+      else {
+        selectedNavIndex = index - 1;
+      } 
+      
+    }
+    else {
+      if (entry.isIntersecting && entry.intersectionRatio > 0) {
+        let maxRatio = entry.intersectionRatio;
+        let id = entry.target.id;
+        if (entry.intersectionRatio > maxRatio) {
+          id = entry.target.id;
+        } 
+
+        const index = sectionIds.indexOf(`#${id}`);
+        selectedNavIndex = index;
+      }
+    }
+
+    activeEffect(navItems[selectedNavIndex]);
+
+
+  });
+}
+
+
+window.addEventListener('wheel', () => {
+  if (window.scrollY === 0) {
+    selectedNavIndex = 0;
+  } 
+});
+
 
 export default Navbar;
