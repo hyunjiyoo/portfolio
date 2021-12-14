7c2db7b1fed4711acdbd3c20b25eb5455796f5e9
hyunjiyoo <solteee23@gmail.com>
2021-12-14 12:10:21 +0900

    about 아이템 크기와 효과 적용

--- src/components/body/about/about.tsx
+++ src/components/body/about/about.tsx
@@ -12,19 +12,19 @@ const About = (): JSX.Element => (
     <div className="container">
       <div className={styles.items}>
         <div className={styles.item}>
-          <div className={styles.subject}>FRONT END</div>
+          <div id={styles.frontend} className={styles.subject}>FRONT END</div>
           <p className={styles.description}>HTML, CSS, Javascript, React, Typescript</p>
         </div>
         <div className={styles.item}>
-          <div className={styles.subject}>INFRA</div>
+          <div id={styles.infra} className={styles.subject}>INFRA</div>
           <p className={styles.description}>Docker</p>
         </div>
         <div className={styles.item}>
-          <div className={styles.subject}>BACK END</div>
+          <div id={styles.backend} className={styles.subject}>BACK END</div>
           <p className={styles.description}>Nodejs, Python</p>
         </div>
         <div className={styles.item}>
-          <div className={styles.subject}>DESIGN</div>
+          <div id={styles.design} className={styles.subject}>DESIGN</div>
           <p className={styles.description}>Photoshop, Illustrator</p>
         </div>
       </div>
