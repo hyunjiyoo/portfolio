b44db144a35593c1504bea40ec82ed1426af7286
hyunjiyoo <solteee23@gmail.com>
2021-12-14 11:45:18 +0900

    about 섹션에 career 추가

--- src/components/body/about/about.tsx
+++ src/components/body/about/about.tsx
@@ -1,4 +1,5 @@
 import Title from '../shared/title/title';
+import Career from './career';
 import styles from './about.module.css';
 
 const About = (): JSX.Element => (
@@ -14,21 +15,22 @@ const About = (): JSX.Element => (
           <div className={styles.subject}>FRONT END</div>
           <p className={styles.description}>HTML, CSS, Javascript, React, Typescript</p>
         </div>
-
         <div className={styles.item}>
           <div className={styles.subject}>INFRA</div>
           <p className={styles.description}>Docker</p>
         </div>
-
         <div className={styles.item}>
           <div className={styles.subject}>BACK END</div>
           <p className={styles.description}>Nodejs, Python</p>
         </div>
-
         <div className={styles.item}>
           <div className={styles.subject}>DESIGN</div>
           <p className={styles.description}>Photoshop, Illustrator</p>
         </div>
+      </div>
+
+      <div className={styles.career}>
+        <Career />
       </div>
     </div>
   </section>
