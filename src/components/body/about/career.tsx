46df5246b16dc0b9f750049bf7e6a2da2af33b5f
hyunjiyoo <solteee23@gmail.com>
2021-12-27 23:33:47 +0900

    커리어 링크 클릭 타겟 변경

--- src/components/body/about/career.tsx
+++ src/components/body/about/career.tsx
@@ -1,26 +1,38 @@
 import { faLink } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useRef } from 'react';
 import styles from './career.module.css';
 
-const Career = () => (
-  <div className={styles.container}>
-    <div className={styles.title}>
-      <span className={styles.decoration}>✔️</span>
-      <img className={styles.logo} src="/images/kirbs_logo.png" alt="kirbs__logo" />
+const Career = () => {
+
+  const aRef = useRef<HTMLAnchorElement>(null);
+
+  const clickLink = () => {
+    const link = aRef.current as HTMLElement;
+    link.click();
+  }
+
+  return (
+    <div className={styles.container} onClick={clickLink}>
+      <div className={styles.title}>
+        <span className={styles.decoration}>✔️</span>
+        <img className={styles.logo} src="/images/kirbs_logo.png" alt="kirbs__logo" />
+      </div>
+      <div className={styles.contents}>
+        <p>[기간] 2019.08 ~ 2021.01 (약 1년 6개월)</p>
+        <p>[업무] 기존 시스템 유지보수 및 신규 프로젝트 개발</p>
+      </div>
+      <a 
+        ref={aRef}
+        className={styles.icon} 
+        href="https://hjyooo.notion.site/3635ae9f47f04b57ba4731d828de225d" 
+        target="_blank" 
+        rel="noreferrer"
+      >
+        <FontAwesomeIcon className={styles.icon__link} icon={faLink} />
+      </a>
     </div>
-    <div className={styles.contents}>
-      <p>[기간] 2019.08 ~ 2021.01 (약 1년 6개월)</p>
-      <p>[업무] 기존 시스템 유지보수 및 신규 프로젝트 개발</p>
-    </div>
-    <a 
-      className={styles.icon} 
-      href="https://hjyooo.notion.site/3635ae9f47f04b57ba4731d828de225d" 
-      target="_blank" 
-      rel="noreferrer"
-    >
-      <FontAwesomeIcon className={styles.icon__link} icon={faLink} />
-    </a>
-  </div>
-);
+  )
+};
 
 export default Career
