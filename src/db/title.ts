d1924ca4bdb8476405e994b10fe5127967a5d842
hyunjiyoo <solteee23@gmail.com>
2021-12-29 14:56:12 +0900

    타이틀 설명 추가

--- src/db/title.ts
+++ src/db/title.ts
@@ -3,32 +3,32 @@ import { SectionKeyType, Title } from './dataStructure';
 export const titleset: Record<SectionKeyType, Title> = {
   'about': {
     name: 'ABOUT ME',
-    description: 'hello about',
+    description: '분야별 주요 기술과 경력에 대해 나타냅니다.',
     position: 'R'
   },
   'note': {
     name: 'MY NOTE',
-    description: 'hello note',
+    description: '노션 어플리케이션을 이용해 메모하며 공부하는 습관을 가지고 있습니다.',
     position: 'L'
   },
   'skills': {
     name: 'SKILLS',
-    description: 'hello note',
+    description: '분야별 기술 수준을 나타냅니다.',
     position: 'R'
   },
   'work': {
     name: 'MY WORK',
-    description: 'hello note',
+    description: '팀 프로젝트 및 개인 프로젝트를 꾸준히 해오고 있습니다.',
     position: 'L'
   },
   'testimonials': {
     name: 'TESTIMONIALS',
-    description: 'hello note',
+    description: '함께 일을 했거나 스터디를 했던 사람들의 코멘트입니다.',
     position: 'R'
   },
   'contact': {
     name: 'CONTACT',
-    description: 'hello note',
+    description: '이 포트폴리오가 마음에 드셨거나 궁금한 점이 있으시다면 연락은 언제나 환영입니다.',
     position: 'L'
   }
 };
