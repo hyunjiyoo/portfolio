9ce97be00d8d23fc0861b1f0920ee2eaee3fff19
hyunjiyoo <hjyoo.dev@gmail.com>
2023-12-03 17:27:41 +0900

    career 및 skill 업데이트

--- src/components/body/about/career.tsx
+++ src/components/body/about/career.tsx
@@ -1,5 +1,5 @@
-import { useRef } from "react";
-import styles from "./career.module.css";
+import { useRef } from 'react';
+import styles from './career.module.css';
 import { BiLinkExternal } from 'react-icons/bi';
 
 const Career = () => {
@@ -8,31 +8,18 @@ const Career = () => {
   const gotoResume = () => {
     const link = aRef.current as HTMLElement;
     link.click();
-  }
+  };
 
   return (
     <div className={styles.container}>
       <div className={styles.logo__wrapper}>
-        <img
-          className={styles.logo}
-          src="/images/career/kirbs_logo.png"
-          alt="kirbs__logo"
-        />
-        <img
-          className={styles.logo}
-          src="/images/career/yogiyo_logo.png"
-          alt="yogiyo__logo"
-        />
+        <img className={styles.logo} src='/images/career/kirbs_logo.png' alt='kirbs__logo' />
+        <img className={styles.logo} src='/images/career/yogiyo_logo.png' alt='yogiyo__logo' />
+        <img className={styles.logo} src='/images/career/nexon_logo.png' alt='nexon__logo' />
       </div>
       <div className={styles.resume}>
         <p onClick={gotoResume}>Go to resume</p>
-        <a
-          ref={aRef}
-          className={styles.icon}
-          href="https://bit.ly/3VxlCE6"
-          target="_blank"
-          rel="noreferrer"
-        >
+        <a ref={aRef} className={styles.icon} href='https://bit.ly/3VxlCE6' target='_blank' rel='noreferrer'>
           <BiLinkExternal className={styles.icon__link} />
         </a>
       </div>
--- src/components/body/skills/skills.tsx
+++ src/components/body/skills/skills.tsx
@@ -3,30 +3,31 @@ import Skill from './skill';
 import styles from './skills.module.css';
 import { SectionKeyType } from '../../../db/dataStructure';
 
-const SECTION_KEY: SectionKeyType = "skills";
+const SECTION_KEY: SectionKeyType = 'skills';
 
 const Skills = (): JSX.Element => (
-  <section id="skills" className={styles.skills}>
+  <section id='skills' className={styles.skills}>
     <Title id={SECTION_KEY} />
     <div className={`container ${styles.skillset}`}>
       <div className={styles.skillset__left}>
         <h2 className={styles.skillset__frontend}>Front-end</h2>
-        <Skill title="CSS" value={99} />
-        <Skill title="Javascript" value={95} />
-        <Skill title="React" value={85} />
-        <Skill title="Typescript" value={80} />
+        <Skill title='CSS' value={99} />
+        <Skill title='Javascript' value={95} />
+        <Skill title='React' value={85} />
+        <Skill title='Vue' value={80} />
+        <Skill title='Typescript' value={80} />
       </div>
 
       <div className={styles.skillset__right}>
         <div className={styles.skillset__backend}>
           <h2>Back-end</h2>
-          <Skill title="Nodejs" value={80} />
-          <Skill title="Python" value={90} />
+          <Skill title='Nodejs' value={80} />
+          <Skill title='Python' value={90} />
         </div>
-        
+
         <div className={styles.skillset__infra}>
           <h2>Infra</h2>
-          <Skill title="Docker" value={90} />
+          <Skill title='Docker' value={90} />
         </div>
       </div>
     </div>
