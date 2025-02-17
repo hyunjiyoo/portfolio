1f8ad7d4a6fede04ae704ba6ddf236ebcab9f5ff
hyunjiyoo <hjyoo.dev@gmail.com>
2025-02-17 15:55:48 +0900

    Modify main title

--- src/components/body/intro/intro.tsx
+++ src/components/body/intro/intro.tsx
@@ -17,10 +17,11 @@ const Intro = ({activeEffect}: IntroProps): JSX.Element => {
   return (
     <section id="intro" className={`${styles.intro} container`}>
       <div className={styles.contents}>
-        <span className={styles.span}>___ Hello Hyunji</span>
+        <span className={styles.span}>홍익인간의 정신으로</span>
         <h1 className={styles.title}>
-          Frontend Developer, <br />
-          Growing Through Habitual Effort
+          널리 세상을 이롭게 하고 싶은 <br />
+          프론트엔드 개발자 <br />
+          유현지입니다
         </h1>
         <button className={styles.button} onClick={moveContactSection}>CONTACT ME</button>
       </div>
